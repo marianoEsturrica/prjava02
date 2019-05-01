973c1940bb37091eb4a88c2fbe4a8d3cb5965cfe
marianoEsturrica <50147970+marianoEsturrica@users.noreply.github.com>
2019-05-01 11:45:09 +0200

    Versió 1 del projecte prjava02

---
+++ src/prjava02/Prjava02.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava02;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author --> Escriu el teu correu electrÃ²nic
+ */
+public class Prjava02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("versiÃ³ 0.1 del projecte prjava02"); 	
+        try {
+            InetAddress adresa = InetAddress.getLocalHost();
+            String hostname = adresa.getHostName();
+            System.out.println("hostname="+hostname);
+            System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+            System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+            System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+            System.out.println("VersiÃ³ OS: " + System.getProperty("os.version"));
+        }
+        catch (IOException e) {
+		System.out.println("Exception occurred");
+        }  
+    }
+}
