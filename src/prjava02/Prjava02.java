3456363339a4123297c36fc0f4fcd292f3f86c48
marianoEsturrica <50147970+marianoEsturrica@users.noreply.github.com>
2019-05-01 12:10:30 +0200

    Treballant amb ordres Git i branques: Versió 1.3 del projecte prjava02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -9,7 +9,7 @@ import java.net.*;
 
 /**
  *
- * @author --> Escriu el teu correu electrÃ²nic
+ * @author --> Escriu el teu correu electrÃƒÂ²nic
  */
 public class Prjava02 {
 
@@ -17,7 +17,7 @@ public class Prjava02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("versiÃ³ 0.1 del projecte prjava02"); 	
+        System.out.println("versiÃƒÂ³ 0.1 del projecte prjava02"); 	
         try {
             InetAddress adresa = InetAddress.getLocalHost();
             String hostname = adresa.getHostName();
@@ -25,9 +25,10 @@ public class Prjava02 {
             System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
-            System.out.println("VersiÃ³ OS: " + System.getProperty("os.version"));
-            System.out.println("Creació d'una branca del projecte prjava02");
-            System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+            System.out.println("VersiÃƒÂ³ OS: " + System.getProperty("os.version"));
+            System.out.println("CreacioÌ� d'una branca del projecte prjava02");
+            System.out.println("Afegint meÌ�s codi a la branca00 del projecte prjava02");
+            System.out.println("Afegint codi a la branca01 del projecte prjava02");
         }
         catch (IOException e) {
 		System.out.println("Exception occurred");
